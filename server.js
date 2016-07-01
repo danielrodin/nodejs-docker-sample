2f04216a9b47037f7778d4d422dbad367410d8bf
Daniel Rodin <Daniel Rodin>
2016-07-01 08:15:01 +0200

    Added a test endpoint

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res){
     res.status(200).send('Hello world with Node.js and Docker');
 })
 
+app.get('/test', function(req, res){
+    res.status(200).send('Testing another endpoint');
+})
+
 var server = app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
