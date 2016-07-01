8536478384d52de37cbd64ef2781ec0156236312
Daniel Rodin <Daniel Rodin>
2016-07-01 08:21:39 +0200

    Fixed async tests

--- spec/server-spec.js
+++ spec/server-spec.js
@@ -6,15 +6,17 @@ var base_url = "http://localhost:5000/";
 
 describe('Root endpoint', function() {
     describe('when requesting', function() {
-        it('it should return OK (200)', function () {
+        it('it should return OK (200)', function (done) {
             request.get(base_url, function(error, response, body) {
                 assert.equal(200, response.statusCode);
+                done();
             });
         });
-        it('it should contain Hello world', function () {
+        it('it should contain Hello world', function (done) {
             request.get(base_url, function(error, response, body) {
                 var isMatch = response.body.indexOf("Hello world") > -1;
-                assert.equal(true, false);
+                assert.equal(isMatch, true);
+                done();
             });
         });
     });
